0ebe0a5350647d62f7cc918c46d46d30d0703c12
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-03-07 14:19:05 +0300

    assignment

---
+++ assignment.js
@@ -0,0 +1,9 @@
+function fruit(){
+    let fruitList=['mango', 'kiwi', 'tomato', 'strawberry', 'guava'];
+    fruitList.forEach(element=>console.log(element));
+    console.log(fruitList[0]);
+
+        
+    }
+
+    fruit();
